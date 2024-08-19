6c56dd3c1f6ccf983ab5a680b224ce5684acb82c
pnhitd <trandophuongnhi111@gmail.com>
2024-08-19 10:26:31 +0700

    updated to deploy

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -27,7 +27,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
     const isFriend  = Array.isArray(friends) && friends.find((friend) => friend._id === friendId);
    
     const patchFriend = async () => {
-        const response = await fetch(`http://localhost:3001/users/${_id}/${friendId}`, {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/users/${_id}/${friendId}`, {
             method: 'PATCH',
             headers: {
                 Authorization: `Bearer ${token}`,
--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -42,7 +42,7 @@ const PostWidget = ({
     const primary = palette.primary.main;
 
     const patchLike = async () => {
-        const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/posts/${postId}/like`, {
             method: 'PATCH',
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -71,7 +71,7 @@ const PostWidget = ({
                     height='auto'
                     alt="post"
                     style={{ borderRadius: '0.75rem', marginTop: '1rem' }}
-                    src={`http://localhost:3001/assets/${picturePath}`}
+                    src={`${process.env.REACT_APP_API_URL}/assets/${picturePath}`}
                 />
             )}
             <FlexBetween mt='0.25rem'>
