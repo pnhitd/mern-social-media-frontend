c84266584f8739b91a5604b606e7460ae19a0683
pnhitd <trandophuongnhi111@gmail.com>
2024-08-24 16:46:21 +0700

    updated .env

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -77,6 +77,7 @@ const Form = () => {
     };
 
     const login = async (values, onSubmitProps) => {
+        console.log(process.env.REACT_APP_API_URL)
         const loggedInResponse = await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {
             method: 'POST',
             headers: {
